196c405a9c920cfc41994ca8fd95767f8bdfabdd
Wesley <matthew282@msn.com>
2022-09-12 21:34:29 -0700

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_11_171316) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_031131) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
