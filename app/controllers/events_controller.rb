519fdacb899a9a2e9bcdedb8a04902db6025a5f2
Wesley <matthew282@msn.com>
2022-09-13 13:35:24 -0700

    add #new and #create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,18 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end 
+
+  def create
+    @event = current_user.build_event(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
