3b9d60a873691218c6cac0cfeb6d99dcb74d9022
Wesley <matthew282@msn.com>
2022-09-14 15:44:24 -0700

    add pages controller and setup homepage / configure root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "events#index"
+  root "pages#index"
   resources :events
 end
