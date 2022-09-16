a75dfbc27e685db91896685ddc1fb83b42c0cd5c
Wesley <matthew282@msn.com>
2022-09-16 15:12:23 -0700

    fix typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+  validates :location, presence: true
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 end
