7b2a84778a57e07364832ad6f9929c21ec793d73
Wesley <matthew282@msn.com>
2022-09-16 14:48:19 -0700

    add location column to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_165612) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_212759) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_16_165612) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.string "location"
   end
 
   create_table "users", force: :cascade do |t|
