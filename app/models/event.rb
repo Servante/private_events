13b8e48c67e80b3ea08b5794e87e611a6fa6316c
Wesley <matthew282@msn.com>
2022-09-17 12:47:53 -0700

    addassociations for invitations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,8 @@ class Event < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
   validates :location, presence: true
+  
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  has_many :invitations, foreign_key: :attended_event_id
+  has_many :attendees, through: :invitations
 end
