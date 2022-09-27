5d80778f5f0a959b7a30212cf11864c254cee236
Wesley <matthew282@msn.com>
2022-09-27 11:25:28 -0700

    accidentl space added/removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_191145) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_190225) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_191145) do
   create_table "invitations", force: :cascade do |t|
     t.integer "event_id"
     t.integer "attendee_id"
-    t.integer "attended_event"
+    t.integer "attended_event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
