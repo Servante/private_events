a5c4846bac78cc78a8b47f4ed5d187c182de7131
Wesley <matthew282@msn.com>
2022-10-13 20:03:43 -0700

    add flash message after successful invite

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,12 @@
 class InvitationsController < ApplicationController
   def create
-    event = Event.find(params[:id])
-    @invitation = event.invitations.build({attendee_id: current_user.id})
+    @event = Event.find(params[:id])
+    @invitation = @event.invitations.build({attendee_id: current_user.id})
 
     if @invitation.save
+      flash[:success] = "You are Invited!"
       current_user.invitations << @invitation
-      redirect_to event
+      redirect_to @event
     else
       render :show, status: :unprocessable_entity
     end
