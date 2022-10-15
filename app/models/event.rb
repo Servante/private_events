e1ff5e0e21b8554e8d46b890380b6d67a9f804cf
Wesley <matthew282@msn.com>
2022-10-14 19:11:56 -0700

    add association destroy dependencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,6 @@ class Event < ApplicationRecord
   
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   # has_many :events, foreign_key: 'event_id'
-  has_many :invitations, foreign_key: :attended_event_id
+  has_many :invitations, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :invitations
 end
