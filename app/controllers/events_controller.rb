b0ae7cb7b92c0015917076dc7ffa1af83333ec14
Wesley <matthew282@msn.com>
2022-10-14 18:05:10 -0700

    add delete event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,8 +36,12 @@ class EventsController < ApplicationController
     end    
   end
 
-  # def destroy
-  # end
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    
+    redirect_to root_path, status: :see_other
+  end
 
   private
     def event_params
