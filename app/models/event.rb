eacb034b2f0f33881b723582dc2191fd159c3e8d
Wesley <matthew282@msn.com>
2022-10-27 10:47:10 -0700

    add class methods .past and .future, restructure html for new logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,15 @@ class Event < ApplicationRecord
   # has_many :events, foreign_key: 'event_id'
   has_many :invitations, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :invitations
+
+
+  def self.past
+    a_events = Event.all
+    p_events = a_events.to_a.keep_if {|event| event.date < Time.now}    
+  end
+
+  def self.future
+    a_events = Event.all
+    p_events = a_events.to_a.keep_if {|event| event.date > Time.now}
+  end
 end
