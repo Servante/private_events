8fe4a9e2e1910a3970fc3bec54ece35b4bbb1d0d
Wesley <matthew282@msn.com>
2022-10-28 11:39:20 -0700

    format syntax for consistency

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,6 +1,6 @@
 class Invitation < ApplicationRecord
-  belongs_to :attendee, class_name: "User"
-  belongs_to :attended_event, class_name: "Event", foreign_key: :attended_event_id 
+  belongs_to :attendee, class_name: 'User'
+  belongs_to :attended_event, class_name: 'Event', foreign_key: :attended_event_id 
   # belongs_to :event, foreign_key: 'event_id'
 end
  
